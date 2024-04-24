e341689f48e923c8db3d800a8829fe4680677944
prachi301s <prachisahu2711@gmail.com>
2024-04-24 23:29:27 +0530

    error solved

--- src/components/table/AdminSubCategoryTable.jsx
+++ src/components/table/AdminSubCategoryTable.jsx
@@ -8,7 +8,7 @@ import { useTheme } from "@emotion/react";
 const AdminSubCategoryTable = () => {
   const { theme } = useTheme();
   const [isModalVisible, setIsModalVisible] = useState(false);
-  const [rows, setRows] = useState();
+ 
   // const { data } = useGetAllSubCategory();
   // console.log(data);
   const handleCreate = () => {
@@ -151,17 +151,17 @@ const AdminSubCategoryTable = () => {
       //   ),
     },
   ];
-  // const rows = [
-  //   { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-  //   { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-  //   { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-  //   { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-  //   { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  //   { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  //   { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  //   { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  //   { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  // ];
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
   return (
     <>
       <div>
--- src/components/table/servicestable/ServicesTable.jsx
+++ src/components/table/servicestable/ServicesTable.jsx
@@ -36,7 +36,7 @@ const StyledSwitch = styled(Switch)({
 });
 
 const ServicesTable = () => {
-  const [rows, setRows] = useState([]);
+ 
   const [isModalVisible, setIsModalVisible] = useState(false);
   // const [isEditMode, setIsEditMode] = useState(false);
   // const [handleAdd, setHandleAdd] = useState();
@@ -373,7 +373,17 @@ const ServicesTable = () => {
       ),
     },
   ];
-
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
   return (
     <>
       <Grid item xs={12} sm={6} md={4} lg={3}>
